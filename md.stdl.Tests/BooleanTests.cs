8b58d636daeadc6691c1383d45c064a2266ee5a9
David Mórász <m@mcro.de>
2017-12-28 15:36:16 +0100

    added XUnit tests

---
+++ md.stdl.Tests/BooleanTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using md.stdl.Boolean;
+
+namespace md.stdl.Tests
+{
+    public class BooleanTests
+    {
+        [Fact]
+        public void SplitJoinTest()
+        {
+            var src = BitUtils.Split(0xFFFFFFFF);
+            Assert.Equal(32, src.Length);
+            Assert.Equal(Enumerable.Repeat(true, 32), src);
+            var res = BitUtils.Join(src);
+            Assert.Equal(0xFFFFFFFF, res);
+        }
+        [Fact]
+        public void OrTest()
+        {
+            Assert.Equal<uint>(0x0, BitUtils.Or(0x0, 0x0, 0x0));
+            Assert.Equal<uint>(0xC0000000, BitUtils.Or(0x80000000, 0x40000000));
+            Assert.Equal<uint>(0xFFFFFFFF, BitUtils.Or(Enumerable.Range(0, 32).Select(val => (uint)0x1 << val).ToArray()));
+        }
+        [Fact]
+        public void AndTest()
+        {
+            Assert.Equal<uint>(0x0, BitUtils.And(0x0, 0x0, 0x0));
+            Assert.Equal<uint>(0x80000008, BitUtils.And(0x80080008, 0x80000008));
+            Assert.Equal<uint>(0x0, BitUtils.And(Enumerable.Range(0, 32).Select(val => (uint)0x1 << val).ToArray()));
+        }
+    }
+}
---
+++ md.stdl.Tests/StringTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using md.stdl.String;
+using Xunit;
+
+namespace md.stdl.Tests
+{
+    public class StringTests
+    {
+        private const string _multiline = "Lorem ipsum\r\ndolor sit amet\r\nconsectetur\r\nand the rest of this nonsense";
+
+        [Fact]
+        public void DiacriticRemoval()
+        {
+            Assert.Equal("Arvizturo tukorfurogep", "Árvíztűrő tükörfúrógép".RemoveDiacritics());
+        }
+
+        [Fact]
+        public void TestLineRange()
+        {
+            Assert.InRange(_multiline.LineRangeFromCharIndex(18).Length, 14, 16);
+        }
+
+        [Fact]
+        public void TestMultiEdit()
+        {
+            var res = _multiline.MultiEdit(new[]
+            {
+                new EditInsert
+                {
+                    InsertText = "Radical",
+                    Length = 5,
+                    Position = 0
+                },
+                new EditInsert
+                {
+                    InsertText = "Awesome",
+                    Length = 5,
+                    Position = 13
+                },
+            });
+            var expected = "Radical ipsum\r\nAwesome sit amet\r\nconsectetur\r\nand the rest of this nonsense";
+            Assert.Equal(expected, res);
+        }
+    }
+}

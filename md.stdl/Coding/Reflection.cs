d8e98063bd3094195fb1165e335db6e1fa90b768
David Mórász <m@mcro.de>
2018-01-28 21:12:53 +0100

    added reflection utils

---
+++ md.stdl/Coding/Reflection.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace md.stdl.Coding
+{
+    public static class ReflectionExtensions
+    {
+        /// <summary>
+        /// Whether object is numeric or not, call for any object
+        /// </summary>
+        public static bool IsNumeric(this object x) { return (x != null && IsNumeric(x.GetType())); }
+
+        /// <summary>
+        /// Whether type is numeric or not
+        /// </summary>
+        public static bool IsNumeric(Type type) { return IsNumeric(type, Type.GetTypeCode(type)); }
+
+        /// <summary>
+        /// Whether the Type and TypeCode pair is numeric or not
+        /// </summary>
+        public static bool IsNumeric(Type type, TypeCode typeCode)
+        {
+            return (typeCode == TypeCode.Decimal ||
+                   (type.IsPrimitive && typeCode !=
+                       TypeCode.Object && typeCode !=
+                       TypeCode.Boolean && typeCode !=
+                       TypeCode.Char
+                   ));
+        }
+
+        /// <summary>
+        /// Get all types this object inherits
+        /// </summary>
+        public static IEnumerable<Type> GetTypes(this Type type)
+        {
+            // is there any base type?
+            if (type == null) yield break;
+            yield return type;
+            if (type.BaseType == null) yield break;
+            // return all implemented or inherited interfaces
+            foreach (var i in type.GetInterfaces())
+            {
+                yield return i;
+            }
+
+            // return all inherited types
+            var currentBaseType = type.BaseType;
+            while (currentBaseType != null)
+            {
+                yield return currentBaseType;
+                currentBaseType = currentBaseType.BaseType;
+            }
+        }
+
+        /// <summary>
+        /// Get name of the type
+        /// </summary>
+        public static string GetName(this Type T, bool full)
+        {
+            if (full) return T.FullName;
+            else return T.Name;
+        }
+    }
+}

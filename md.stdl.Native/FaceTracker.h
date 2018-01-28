f62c2c7fc389f2aa5b184b882a19783094614782
David Mórász <m@mcro.de>
2018-01-28 23:02:37 +0100

    making md.stdl.Native work with nuget and appveyor

--- md.stdl.Native/FaceTracker.h
+++ md.stdl.Native/FaceTracker.h
@@ -2,6 +2,12 @@
 
 #include "stdafx.h"
 
+#if defined(_WIN64)
+#define PLATFORM x64
+#else
+#define PLATFORM x86
+#endif
+
 using namespace System;
 using namespace System::IO;
 using namespace System::Collections::Generic;
@@ -91,7 +97,7 @@ namespace mp {
 
 				void handleResult(int * pResult);
 
-				[DllImport("libfacedetect.dll", EntryPoint = "?facedetect_frontal@@YAPEAHPEAE0HHHMHHHH@Z")]
+				[DllImport("libfacedetect-PLATFORM.dll", EntryPoint = "?facedetect_frontal@@YAPEAHPEAE0HHHMHHHH@Z")]
 				static int * facedetect_frontal(unsigned char * result_buffer, //buffer memory for storing face detection results, !!its size must be 0x20000 Bytes!!
 					unsigned char * gray_image_data, int width, int height, int step, //input image, it must be gray (single-channel) image!
 					float scale, //scale factor for scan windows
@@ -100,7 +106,7 @@ namespace mp {
 					int max_object_width, //Maximum possible face size. Faces larger than that are ignored. It is the largest posible when max_object_width=0.
 					int doLandmark); // landmark detection
 
-				[DllImport("libfacedetect.dll", EntryPoint = "?facedetect_frontal_surveillance@@YAPEAHPEAE0HHHMHHHH@Z")]
+				[DllImport("libfacedetect-PLATFORM.dll", EntryPoint = "?facedetect_frontal_surveillance@@YAPEAHPEAE0HHHMHHHH@Z")]
 				static int * facedetect_frontal_surveillance(unsigned char * result_buffer, //buffer memory for storing face detection results, !!its size must be 0x20000 Bytes!!
 					unsigned char * gray_image_data, int width, int height, int step, //input image, it must be gray (single-channel) image!
 					float scale, //scale factor for scan windows
@@ -109,7 +115,7 @@ namespace mp {
 					int max_object_width, //Maximum possible face size. Faces larger than that are ignored. It is the largest posible when max_object_width=0.
 					int doLandmark); // landmark detection
 
-				[DllImport("libfacedetect.dll", EntryPoint = "?facedetect_multiview@@YAPEAHPEAE0HHHMHHHH@Z")]
+				[DllImport("libfacedetect-PLATFORM.dll", EntryPoint = "?facedetect_multiview@@YAPEAHPEAE0HHHMHHHH@Z")]
 				static int * facedetect_multiview(unsigned char * result_buffer, //buffer memory for storing face detection results, !!its size must be 0x20000 Bytes!!
 					unsigned char * gray_image_data, int width, int height, int step, //input image, it must be gray (single-channel) image!
 					float scale, //scale factor for scan windows
@@ -118,7 +124,7 @@ namespace mp {
 					int max_object_width, //Maximum possible face size. Faces larger than that are ignored. It is the largest posible when max_object_width=0.
 					int doLandmark); // landmark detection
 
-				[DllImport("libfacedetect.dll", EntryPoint = "?facedetect_multiview_reinforce@@YAPEAHPEAE0HHHMHHHH@Z")]
+				[DllImport("libfacedetect-PLATFORM.dll", EntryPoint = "?facedetect_multiview_reinforce@@YAPEAHPEAE0HHHMHHHH@Z")]
 				static int * facedetect_multiview_reinforce(unsigned char * result_buffer, //buffer memory for storing face detection results, !!its size must be 0x20000 Bytes!!
 					unsigned char * gray_image_data, int width, int height, int step, //input image, it must be gray (single-channel) image!
 					float scale, //scale factor for scan windows

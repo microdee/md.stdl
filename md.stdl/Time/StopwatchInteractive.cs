d5020228f92a5012a12c3ab9533e5238849557bf
David Mórász <m@mcro.de>
2018-02-08 23:20:59 +0100

    added stopwatch variant where you can set elapsed time

---
+++ md.stdl/Time/StopwatchInteractive.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace md.stdl.Time
+{
+    public class StopwatchInteractive : Stopwatch
+    {
+        private TimeSpan _timeOffset = TimeSpan.Zero;
+
+        public new TimeSpan Elapsed => base.Elapsed + _timeOffset;
+        public new long ElapsedMilliseconds => base.ElapsedMilliseconds + (long)_timeOffset.TotalMilliseconds;
+        public new long ElapsedTicks => base.ElapsedTicks + _timeOffset.Ticks;
+
+        public void SetTime(TimeSpan time)
+        {
+            _timeOffset = time;
+            if (IsRunning)
+            {
+                Restart();
+            }
+            else
+            {
+                Reset();
+            }
+        }
+    }
+}

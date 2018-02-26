c18363c4e8a8c06c200dde4c9bf42ae5526b9add
David Mórász <m@mcro.de>
2018-02-26 15:09:01 +0100

    attempt to fix sliding behavior

--- md.stdl/Interaction/Notui/Behaviors/Sliding.cs
+++ md.stdl/Interaction/Notui/Behaviors/Sliding.cs
@@ -175,7 +175,7 @@ namespace md.stdl.Interaction.Notui.Behaviors
 
         private SelectedPlane _actualPlaneSelection;
 
-        private Vector3 GetTouchWorldVelocity(TouchContainer touch, Matrix4x4 plane, NotuiContext context, out Vector3 currpos, out Vector3 prevpos)
+        private Vector3 GetTouchPlanarVelocity(TouchContainer touch, Matrix4x4 plane, NotuiContext context, out Vector3 currpos, out Vector3 prevpos)
         {
             // get planar coords for current touch position
             var hit = Intersections.PlaneRay(touch.WorldPosition, touch.ViewDir, plane, out var capos, out var crpos);
@@ -235,8 +235,7 @@ namespace md.stdl.Interaction.Notui.Behaviors
                         Matrix4x4.Decompose(invparenttr, out var ipscale, out var iprot, out var ippos);
                         worldrot = worldrot * iprot;
                     }
-                    element.DisplayTransformation.GlobalRotate(worldrot);
-                    element.UpdateFromDisplayToInteraction(element);
+                    disptr.GlobalRotate(worldrot);
                 }
             }
             element.UpdateFromDisplayToInteraction(element);
@@ -245,7 +244,6 @@ namespace md.stdl.Interaction.Notui.Behaviors
         private void FlickProgress(BehaviorState state, NotuiContext context)
         {
             var frametime = context.DeltaTime * FlickTime;
-            var fade = Min(Max(1 / frametime, 0), 1);
             if (FlickTime < context.DeltaTime)
             {
                 state.DeltaPos = Vector2.Zero;
@@ -254,9 +252,9 @@ namespace md.stdl.Interaction.Notui.Behaviors
             }
             else
             {
-                state.DeltaPos = Vector2.Lerp(state.DeltaPos, Vector2.Zero, fade);
-                state.DeltaAngle *= fade;
-                state.DeltaSize *= fade;
+                state.DeltaPos = Filters.Velocity(state.DeltaPos, Vector2.Zero, frametime);
+                state.DeltaAngle = Filters.Velocity(state.DeltaAngle, 0, frametime);
+                state.DeltaSize = Filters.Velocity(state.DeltaSize, 0, frametime);
             }
         }
 
@@ -270,45 +268,21 @@ namespace md.stdl.Interaction.Notui.Behaviors
         }
 
         /// <summary>
-        /// This was "stolen" from elliot woods. I'm not sure how it works but it does.
+        /// Calculate deltas from 2 touches and their previous state
         /// </summary>
         /// <param name="curr0">Current first point</param>
         /// <param name="prev0">Previous first point</param>
         /// <param name="curr1">Current second point</param>
         /// <param name="prev1">Previos second point</param>
-        /// <param name="tr">Outpur matrix</param>
         /// <returns>Delta XY, delta angle, delta scale</returns>
-        private Vector4 CalcDeltaMatrix(Vector2 curr0, Vector2 prev0, Vector2 curr1, Vector2 prev1, out Matrix4x4 tr)
+        private Vector4 CalcDeltaFromTwoTouch(Vector2 curr0, Vector2 prev0, Vector2 curr1, Vector2 prev1)
         {
-            var prev0x = prev0.X;
-            var prev0y = prev0.Y;
-            var prev1x = prev1.X;
-            var prev1y = prev1.Y;
-            var curr0x = curr0.X;
-            var curr0y = curr0.Y;
-            var curr1x = curr1.X;
-            var curr1y = curr1.Y;
-            var prevdiffx = prev1x - prev0x;
-            var prevdiffy = prev1y - prev0y;
-            var currdiffx = curr1x - curr0x;
-            var currdiffy = curr1y - curr0y;
-            //Sqrt((currdiffx * currdiffx + currdiffy * currdiffy) / (prevdiffx * prevdiffx + prevdiffy * prevdiffy));
-            var rot1 = (prevdiffx * currdiffx + prevdiffy * currdiffy) / (prevdiffx * prevdiffx + prevdiffy * prevdiffy);
-            var rot2 = (prevdiffy * currdiffx - prevdiffx * currdiffy) / (prevdiffx * prevdiffx + prevdiffy * prevdiffy);
-            var tx = curr0x - (rot1 * prev0x + rot2 * prev0y);
-            var ty = curr0y - (-rot2 * prev0x + rot1 * prev0y);
-
-            var restr = new Matrix4x4(
-                rot1, -rot2, 0.0f, 0.0f,
-                rot2,  rot1, 0.0f, 0.0f,
-                0.0f,  0.0f, 1.0f, 0.0f,
-                tx,    ty,   0.0f, 1.0f
-            );
-
-            tr = restr;
-            var refv = Vector4.Transform(Vector4.UnitX, restr).xy();
-            var polar = Coordinates.RectToPolar(refv);
-            return new Vector4(tx, ty, polar.X, polar.Y);
+            var currpolar = Coordinates.RectToPolar(curr0 - curr1);
+            var prevpolar = Coordinates.RectToPolar(prev0 - prev1);
+            var curravg = (curr0 + curr1) / 2;
+            var prevavg = (prev0 + prev1) / 2;
+            
+            return new Vector4(curravg - prevavg, currpolar.X - prevpolar.X, currpolar.Y - prevpolar.Y);
         }
 
         public override void Behave(NotuiElement element)
@@ -353,7 +327,7 @@ namespace md.stdl.Interaction.Notui.Behaviors
                 // if Draggable is true and there's only 1 touch do a simple translation move only
                 if (Draggable && element.Touching.Count == 1)
                 {
-                    var relvel = GetTouchWorldVelocity(element.Touching.Keys.First(), usedplane, element.Context,
+                    var relvel = GetTouchPlanarVelocity(element.Touching.Keys.First(), usedplane, element.Context,
                         out var crelpos, out var prelpos);
                     currstate.DeltaPos = relvel.xy();
 
@@ -371,9 +345,9 @@ namespace md.stdl.Interaction.Notui.Behaviors
                 // If Draggable is off but Pivotable or Scalable is still on do a rotation around the element center
                 if (!Draggable && element.Touching.Count == 1)
                 {
-                    GetTouchWorldVelocity(element.Touching.Keys.First(), usedplane, element.Context,
+                    GetTouchPlanarVelocity(element.Touching.Keys.First(), usedplane, element.Context,
                         out var crelpos, out var prelpos);
-                    var deltarn = CalcDeltaMatrix(crelpos.xy(), prelpos.xy(), crelpos.xy() * -1, prelpos.xy() * -1, out var deltarntr);
+                    var deltarn = CalcDeltaFromTwoTouch(crelpos.xy(), prelpos.xy(), crelpos.xy() * -1, prelpos.xy() * -1);
 
                     // reset translation delta
                     currstate.DeltaPos = Vector2.Zero;
@@ -392,9 +366,9 @@ namespace md.stdl.Interaction.Notui.Behaviors
                 var orderedbyfastest = touches.OrderByDescending(t => t.Velocity.LengthSquared()).ToArray();
                 var t0 = orderedbyfastest[0];
                 var t1 = orderedbyfastest[1];
-                GetTouchWorldVelocity(t0, usedplane, element.Context, out var cp0, out var pp0);
-                GetTouchWorldVelocity(t1, usedplane, element.Context, out var cp1, out var pp1);
-                var delta = CalcDeltaMatrix(cp0.xy(), pp0.xy(), cp1.xy(), pp1.xy(), out var deltatr);
+                GetTouchPlanarVelocity(t0, usedplane, element.Context, out var cp0, out var pp0);
+                GetTouchPlanarVelocity(t1, usedplane, element.Context, out var cp1, out var pp1);
+                var delta = CalcDeltaFromTwoTouch(cp0.xy(), pp0.xy(), cp1.xy(), pp1.xy());
 
                 currstate.DeltaPos = delta.xy();
                 currstate.DeltaAngle = delta.Z;

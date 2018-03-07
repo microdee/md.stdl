03bf4f48c1ec17c41ab54df4071eed58179f2909
David Mórász <m@mcro.de>
2018-03-07 15:24:01 +0100

    added some common interfaces

---
+++ md.stdl/Interfaces/IMainlooping.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace md.stdl.Interfaces
+{
+    public interface IMainlooping
+    {
+        /// <summary>
+        /// First thing should be invoked in the mainloop
+        /// </summary>
+        event EventHandler OnMainLoopBegin;
+
+        /// <summary>
+        /// Last thing should be invoked in the mainloop
+        /// </summary>
+        event EventHandler OnMainLoopEnd;
+
+        /// <summary>
+        /// Function to be called once every frame
+        /// </summary>
+        /// <param name="deltatime">Delta time between frames usually in seconds but can be ms (depending on the implementer)</param>
+        void Mainloop(float deltatime);
+    }
+}

bf9f08594d66cef543a0dc3a5c711b93dce284a5
David Mórász <m@mcro.de>
2018-04-19 21:16:49 +0200

    Added elapsed time based triggers

--- md.stdl/Time/StopwatchInteractive.cs
+++ md.stdl/Time/StopwatchInteractive.cs
@@ -4,13 +4,16 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using md.stdl.Interfaces;
 
 namespace md.stdl.Time
 {
+    /// <inheritdoc cref="Stopwatch" />
+    /// <inheritdoc cref="IMainlooping" />
     /// <summary>
-    /// A Seekable version of Stopwatch
+    /// A Seekable version of Stopwatch with the capability to fire time based triggers synchronously
     /// </summary>
-    public class StopwatchInteractive : Stopwatch
+    public class StopwatchInteractive : Stopwatch, IMainlooping
     {
         private TimeSpan _timeOffset = TimeSpan.Zero;
 
@@ -29,6 +32,46 @@ namespace md.stdl.Time
             {
                 Reset();
             }
+        }
+
+        public event EventHandler OnMainLoopBegin;
+        public event EventHandler OnMainLoopEnd;
+        public event EventHandler OnTriggerPassed;
+
+        private readonly Dictionary<TimeSpan, bool> _triggers = new Dictionary<TimeSpan, bool>();
+
+        public void SetTrigger(params TimeSpan[] triggers)
+        {
+            _triggers.Clear();
+            foreach (var time in triggers)
+            {
+                if(_triggers.ContainsKey(time)) continue;
+                _triggers.Add(time, Elapsed >= time);
+            }
+        }
+
+        public void ResetTriggers()
+        {
+            foreach (var time in _triggers.Keys)
+            {
+                _triggers[time] = false;
+            }
+        }
+
+        public void Mainloop(float deltatime)
+        {
+            OnMainLoopBegin?.Invoke(this, EventArgs.Empty);
+            
+            foreach (var trigger in _triggers.Keys)
+            {
+                if (Elapsed >= trigger && !_triggers[trigger])
+                {
+                    OnTriggerPassed?.Invoke(this, EventArgs.Empty);
+                    _triggers[trigger] = true;
+                }
+            }
+
+            OnMainLoopEnd?.Invoke(this, EventArgs.Empty);
         }
     }
 }

0bd831d9437c8def9f933d12f1a20b7e72d8d98c
David Mórász <m@mcro.de>
2018-05-17 14:17:36 +0200

    Added MainloopScheduler to observe data on your mainloop. Ruthlessly stolen from vvvv-sdk

---
+++ md.stdl.Tests/MainloopSchedulerTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using md.stdl.Interfaces;
+using Xunit;
+
+namespace md.stdl.Tests
+{
+    public class MainloopSchedulerTest
+    {
+        private int _currval = -1;
+
+        [Fact]
+        public void Test()
+        {
+            var array = new[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+            var scheduler = new MainloopScheduler();
+            array.ToObservable().ObserveOn(scheduler).Subscribe(v => _currval = v);
+            for (int i = 0; i < array.Length*3; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    scheduler.Mainloop();
+                    Assert.Equal(i/3, _currval);
+                }
+            }
+        }
+    }
+}

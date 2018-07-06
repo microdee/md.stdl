a60673869aa47cd49e57bef1767d9c2f49b32a34
David Mórász <m@mcro.de>
2018-07-06 21:35:03 +0200

    Added couple of extension methods operating on Attributes

---
+++ md.stdl.Tests/ReflectionTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using md.stdl.Coding;
+using Xunit;
+
+namespace md.stdl.Tests
+{
+    [AttributeUsage(AttributeTargets.All)]
+    public class AssignToMeAttribute : Attribute
+    {
+        public string Name;
+
+        public AssignToMeAttribute(string n)
+        {
+            Name = n;
+        }
+    }
+
+    public abstract class ReflectionTestBase
+    {
+        [AssignToMe("Another Target")]
+        public string AnotherAssignee { get; set; } = "nope";
+    }
+
+    public class ReflectionTests : ReflectionTestBase
+    {
+        [AssignToMe("Target")]
+        public string Assignee { get; set; } = "nope";
+
+        [AssignToMe("Yet Another Target")]
+        public string FromInterface { get; set; } = "nope";
+
+        [Fact]
+        public void IsNumericTest()
+        {
+            object o = 7.0;
+            Assert.True(o.IsNumeric());
+            o = (int) 7;
+            Assert.True(o.IsNumeric());
+            Assert.True(typeof(decimal).IsNumeric());
+            o = new List<int>() {0, 1, 2};
+            Assert.False(o.IsNumeric());
+            Assert.False(typeof(ReflectionTests).IsNumeric());
+        }
+
+        [Fact]
+        public void IsTest()
+        {
+            Assert.True(typeof(List<int>).Is(typeof(IList<int>)));
+        }
+
+        [Fact]
+        public void AttributeConditionalPropertyAssignmentTest()
+        {
+            var result = this.AttributeConditionalPropertyAssignment<AssignToMeAttribute>(
+                "yarp", attr => attr.Name.Equals("Target"), inherit: true);
+            Assert.True(result);
+            Assert.Equal("yarp", Assignee);
+            Assert.Equal("nope", AnotherAssignee);
+            Assert.Equal("nope", FromInterface);
+
+            result = this.AttributeConditionalPropertyAssignment<AssignToMeAttribute>(
+                "yarp", attr => attr.Name.Equals("Another Target"), inherit: true);
+            Assert.True(result);
+            Assert.Equal("yarp", AnotherAssignee);
+            Assert.Equal("nope", FromInterface);
+        }
+    }
+}
--- md.stdl/Coding/Reflection.cs
+++ md.stdl/Coding/Reflection.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,7 +15,7 @@ namespace md.stdl.Coding
         /// <summary>
         /// Whether object is numeric or not, call for any object
         /// </summary>
-        public static bool IsNumeric(this object x) { return x == null ? false : IsNumeric(x.GetType()); }
+        public static bool IsNumeric(this object x) { return x != null && IsNumeric(x.GetType()); }
 
         /// <summary>
         /// Whether type is numeric or not
@@ -76,6 +77,111 @@ namespace md.stdl.Coding
         {
             if (full) return T.FullName;
             else return T.Name;
+        }
+
+        /// <summary>
+        /// Assign an object to a member based on a predicate function operating on member attributes
+        /// </summary>
+        /// <typeparam name="TAttr">Type of attribute</typeparam>
+        /// <param name="target">Object containing the target property</param>
+        /// <param name="source">Desired value of the target property</param>
+        /// <param name="predicate">Filter for attribute</param>
+        /// <param name="inherit">Take custom attributes inheritance into account. False by default</param>
+        /// <param name="enumerableFill">If target is enumerable then fill with delegate. if null enumerables are ignored</param>
+        /// <returns>True if everything was fine</returns>
+        public static bool AttributeConditionalFieldAssignment<TAttr>(this object target, object source,
+            Func<TAttr, bool> predicate, bool inherit = false,
+            Action<object, object, FieldInfo> enumerableFill = null) where TAttr : Attribute
+        {
+            try
+            {
+                var field =
+                (
+                    from f in target.GetType().GetFields()
+                    where f.GetCustomAttributes(typeof(TAttr), inherit).Length > 0
+                    let attr = (TAttr)(f.GetCustomAttributes(typeof(TAttr), inherit)[0])
+                    where predicate(attr)
+                    select f
+                ).First();
+                if (field.FieldType.IsEnumerable())
+                {
+                    enumerableFill?.Invoke(target, source, field);
+                    return true;
+                }
+                else
+                {
+                    field.SetValue(target, source);
+                    return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Assign an object to a member based on a predicate function operating on member attributes
+        /// </summary>
+        /// <typeparam name="TAttr">Type of attribute</typeparam>
+        /// <param name="target">Object containing the target property</param>
+        /// <param name="source">Desired value of the target property</param>
+        /// <param name="predicate">Filter for attribute</param>
+        /// <param name="inherit">Take custom attributes inheritance into account. False by default</param>
+        /// <param name="enumerableFill">If target is enumerable then fill with delegate. if null enumerables are ignored</param>
+        /// <returns>True if everything was fine</returns>
+        public static bool AttributeConditionalPropertyAssignment<TAttr>(this object target, object source,
+            Func<TAttr, bool> predicate, bool inherit = false,
+            Action<object, object, PropertyInfo> enumerableFill = null) where TAttr : Attribute
+        {
+            try
+            {
+                var prop =
+                (
+                    from p in target.GetType().GetProperties()
+                    where p.GetCustomAttributes(typeof(TAttr), inherit).Length > 0
+                    let attr = (TAttr)(p.GetCustomAttributes(typeof(TAttr), inherit)[0])
+                    where predicate(attr)
+                    select p
+                ).First();
+                if (prop.PropertyType.IsEnumerable())
+                {
+                    enumerableFill?.Invoke(target, source, prop);
+                    return true;
+                }
+                else
+                {
+                    prop.SetValue(target, source);
+                    return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Shortcut to get the custom attributes of a property
+        /// </summary>
+        /// <typeparam name="TAttr">Type of attribute</typeparam>
+        /// <param name="target">Object containing the target property</param>
+        /// <param name="name">The name of the target property</param>
+        /// <param name="inherit">Take custom attributes inheritance into account. False by default</param>
+        /// <returns>IEnumerable of attributes</returns>
+        public static IEnumerable<TAttr> GetAttributesOfProperty<TAttr>(this object target, string name, bool inherit = false) where TAttr : Attribute
+        {
+            return target.GetType().GetProperty(name)?.GetCustomAttributes(typeof(TAttr), inherit).Cast<TAttr>() ?? Enumerable.Empty<TAttr>();
+        }
+
+        /// <summary>
+        /// Is type enumerable?
+        /// </summary>
+        /// <param name="t"></param>
+        /// <returns></returns>
+        public static bool IsEnumerable(this Type t)
+        {
+            return t.GetInterface("IEnumerable") != null && t != typeof(string);
         }
     }
 }

42020fc6f952b75ed5175b5993857223e1c555df
David Morasz <morasz@meso.net>
2018-08-31 15:39:39 +0200

    removed native project and the initial facetracker from mp.dx

--- md.stdl.Native/Stdafx.cpp
+++
@@ -1,5 +0,0 @@
-// stdafx.cpp : source file that includes just the standard includes
-// md.stdl.Native.pch will be the pre-compiled header
-// stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"

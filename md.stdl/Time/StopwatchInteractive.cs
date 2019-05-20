f94aa94a5b98c6ddb15fd226400116a0176e1529
David Morasz <morasz@meso.net>
2019-05-20 15:50:34 +0200

    added internal mainloop support for StopwatchInteractive

--- md.stdl/Time/StopwatchInteractive.cs
+++ md.stdl/Time/StopwatchInteractive.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 using md.stdl.Interfaces;
 
 namespace md.stdl.Time
@@ -13,9 +14,10 @@ namespace md.stdl.Time
     /// <summary>
     /// A Seekable version of Stopwatch with the capability to fire time based triggers synchronously
     /// </summary>
-    public class StopwatchInteractive : Stopwatch, IMainlooping
+    public class StopwatchInteractive : Stopwatch, IMainlooping, IDisposable
     {
         private TimeSpan _timeOffset = TimeSpan.Zero;
+        private Timer _timer;
 
         /// <inheritdoc cref="Stopwatch"/>
         public new TimeSpan Elapsed => base.Elapsed + _timeOffset;
@@ -40,6 +42,30 @@ namespace md.stdl.Time
                 Reset();
             }
         }
+
+        /// <summary>
+        /// Set time immediately fluidly
+        /// </summary>
+        /// <param name="time"></param>
+        public StopwatchInteractive WithTime(TimeSpan time)
+        {
+            SetTime(time);
+            return this;
+        }
+
+        /// <summary>
+        /// Initialize this interactive stopwach with a timer which fires the mainloop at given interval in ms
+        /// </summary>
+        /// <param name="interval">Timer period in ms</param>
+        /// <returns></returns>
+        public StopwatchInteractive WithInternalMainloop(double interval = 10.0)
+        {
+            _timer = new Timer(interval) { AutoReset = true };
+            _timer.Elapsed += (sender, args) => Mainloop(0);
+            _timer.Start();
+            return this;
+        }
+
         /// <inheritdoc cref="IMainlooping"/>
         public event EventHandler OnMainLoopBegin;
         /// <inheritdoc cref="IMainlooping"/>
@@ -63,6 +89,16 @@ namespace md.stdl.Time
             {
                 _triggers.Add((time, false));
             }
+        }
+
+        /// <summary>
+        /// Fluidly set a list of triggers when the OnTriggerPassed should be invoked
+        /// </summary>
+        /// <param name="triggers"></param>
+        public StopwatchInteractive WithTriggers(params TimeSpan[] triggers)
+        {
+            SetTrigger(triggers);
+            return this;
         }
 
         /// <summary>
@@ -96,6 +132,12 @@ namespace md.stdl.Time
             }
 
             OnMainLoopEnd?.Invoke(this, EventArgs.Empty);
+        }
+
+        /// <inheritdoc cref="IDisposable"/>
+        public void Dispose()
+        {
+            _timer?.Dispose();
         }
     }
 }
